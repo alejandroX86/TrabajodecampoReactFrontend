52b2585609119f9b0e5e68e319df9551b500818a
Omar Alejandro Bazar <69990247+alejandroX86@users.noreply.github.com>
2024-03-26 11:58:54 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{ marginTop: "1px" }}>
+        <h1 style={{ textAlign: "left" }} >HOTEL&nbsp; JCP</h1>
+         <h2 style={{ marginTop: "2px" }}>
+         trabajo de Campo
+         </h2>
+        
+         <h4 style={{ textAlign: "left" }}>Julia Avalos</h4>
+          <h4 style={{ textAlign: "left" }}>Leonardo Caceres</h4>
+          <h4 style={{ textAlign: "left" }}>Omar Bazar</h4>
+
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
